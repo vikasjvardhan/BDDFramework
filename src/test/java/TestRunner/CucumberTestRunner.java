2f4209084c2e39089a0a27566bb39bfcd5ab8f7a
vikas <vikasvardhan@gmail.com>
2026-08-10 17:25:07 +0530

    done

--- src/test/java/TestRunner/CucumberTestRunner.java
+++ src/test/java/TestRunner/CucumberTestRunner.java
@@ -5,7 +5,10 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		features = "src/test/resources/Features",
 		glue = {"StepDefinitions"},
-		plugin = {"pretty", "html:target/cucumber-reports.html"}
+		plugin = {"pretty", "html:target/cucumber-reports.html",
+				"json:target/cucumber-reports/CucumberTestReport.json"
+				}
+		
 		//tags = "@LoginTest"
 		 
 )
